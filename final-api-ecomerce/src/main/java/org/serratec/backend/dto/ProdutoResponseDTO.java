17f93fadb4cacdb21ea0dd21e2ce90d76f389645
eduardhafecher <eduardha.alves@gmail.com>
2025-05-31 01:32:31 -0300

    Criei Produto Repository, ProdutoResponseDTO, ProdutoRequestDTO, ProdutoException (e adicionei o BAD REQUEST do produto na classe Exception Handler), ProdutoService e ProdutoController

---
+++ final-api-ecomerce/src/main/java/org/serratec/backend/dto/ProdutoResponseDTO.java
@@ -0,0 +1,17 @@
+package org.serratec.backend.dto;
+
+import org.serratec.backend.entity.Produto;
+
+public record ProdutoResponseDTO(Long id, String nome, Double preco) {
+
+    public ProdutoResponseDTO(Produto produto) {
+        this.id = produto.getId();
+        this.nome = produto.getNome();
+        this.preco = produto.getPreco();
+        if (produto.getCategoria() != null) {
+        }
+            //ele vai apontar erro aqui até que a Categoria Response esteja criada
+           this.categoria = new CategoriaResponseDTO(produto.getCategoria());
+        }
+
+}
